6ec60d3c4179f279d3a336d016c829c5073aeee3
divya pargave <divyanginipargave@gmail.com>
2025-12-12 12:48:54 +0530

    city

---
+++ ASS2/q4/mainnnn.py
@@ -0,0 +1,11 @@
+import requests
+api_key="96b7252e077dc1e9f82294c7fa7cb2cd"
+city = input("Enter city: ")
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+response = requests.get(url)
+print("status:", response.status_code)
+weather = response.json()
+# print(weather)
+print("Temperature: ", weather["main"]["temp"])
+print("Humidity: ", weather["main"]["humidity"])
+print("Wind Speed: ", weather["wind"]["speed"])
